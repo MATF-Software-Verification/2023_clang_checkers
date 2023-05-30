3842204c834a18241547a2620808b76bbcab43f9
Lucija Milicic <74015335+lucijamilicic@users.noreply.github.com>
2023-05-30 17:27:41 +0200

    new tests

---
+++ InfiniteLoopChecker/Tests/11.cpp
@@ -0,0 +1,16 @@
+void test() {
+  int x = 1;
+
+
+    if(x){
+     
+        while(1){ // expected warning: infinite loop
+
+        }
+        
+    }else{
+        x++;
+    }
+
+
+}
---
+++ InfiniteLoopChecker/Tests/12.cpp
@@ -0,0 +1,13 @@
+void test() {
+  
+  for(int i = 0; i < 5; i++){
+
+    while(1){
+
+    }
+
+    
+  }
+
+
+}
